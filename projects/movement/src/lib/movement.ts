3574d18e60c79fe9ab7843bd7c363ca7886c12cd
andersseen <andriipap01@gmail.com>
2026-03-10 17:23:05 +0100

    setup project

---
+++ projects/movement/src/lib/movement.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'move-movement',
+  imports: [],
+  template: ` <p>movement works!</p> `,
+  styles: ``,
+})
+export class Movement {}
